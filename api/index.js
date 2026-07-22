ff560f2b5af74dc32aaa6b11c9dc7edcad54ee6c
Mohammed Rakshan <mohammedrakshan@Mohammeds-MacBook-Air.local>
2026-07-22 17:06:36 +0530

    Use direct Express export for Vercel

--- api/index.js
+++ api/index.js
@@ -1,13 +1,3 @@
 const app = require('../app');
 
-module.exports = async function handler(req, res) {
-  const { createServer } = require('http');
-  const server = createServer(app);
-
-  return new Promise((resolve, reject) => {
-    server.once('error', reject);
-    server.emit('request', req, res);
-    server.once('listening', () => resolve(undefined));
-    server.listen(0, '127.0.0.1');
-  });
-};
+module.exports = app;
