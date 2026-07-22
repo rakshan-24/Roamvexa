3f790b7f6e4e1df975f7b2bb90b63ad40aac5b4b
Mohammed Rakshan <mohammedrakshan@Mohammeds-MacBook-Air.local>
2026-07-22 16:59:04 +0530

    Switch Vercel function to Node runtime

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
-const serverless = require('serverless-http');
 const app = require('../app');
 
-module.exports = serverless(app);
+module.exports = async function handler(req, res) {
+  const { createServer } = require('http');
+  const server = createServer(app);
+
+  return new Promise((resolve, reject) => {
+    server.once('error', reject);
+    server.emit('request', req, res);
+    server.once('listening', () => resolve(undefined));
+    server.listen(0, '127.0.0.1');
+  });
+};
